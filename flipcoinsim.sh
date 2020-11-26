de11467c3c0e52b410ce2bd447469f6402e7854d
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2020-11-27 03:51:46 +0530

    Added UC1

--- flipcoinsim.sh
+++ flipcoinsim.sh
@@ -5,3 +5,16 @@ head=1
 
 read -p "count the flip cion : " flipCount
 
+for (( i=0 ; i< $flipCount ; i++ ))
+do
+	flipCoinResult=$((RANDOM%2))
+	if [[ $flipCoinResult == $tail ]]
+	then
+        	echo "Tail-$flipCoinResult Winner"
+	elif [[ $flipCoinResult == $head ]]
+	then
+		echo "Head-$flipCoinResult Winner"
+	else
+		echo "Invalid Result"
+	fi
+done
