b840f7a9c3579d952a717d65ed3a82e080bf8b1c
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2020-11-27 04:00:42 +0530

    Added UC2

--- flipcoinsim.sh
+++ flipcoinsim.sh
@@ -1,20 +1,26 @@
+
 #!/bin/bash
 
 tail=0
 head=1
+headCount=0
+tailCount=0
 
-read -p "count the flip cion : " flipCount
-
+read -p "How many times you want to flip a coin:" flipCount
 for (( i=0 ; i< $flipCount ; i++ ))
 do
 	flipCoinResult=$((RANDOM%2))
 	if [[ $flipCoinResult == $tail ]]
 	then
         	echo "Tail-$flipCoinResult Winner"
+		((tailCount++))
 	elif [[ $flipCoinResult == $head ]]
 	then
 		echo "Head-$flipCoinResult Winner"
+		((headCount++))
 	else
 		echo "Invalid Result"
 	fi
 done
+echo "Tail win $tailCount times"
+echo "Head win $headCount times"
